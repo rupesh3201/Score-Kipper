b4ac1040a9d9a26fc239c8078bb5582d2dc46b38
rupesh3201 <rupesh.mandge3201@gmail.com>
2024-10-02 15:00:03 +0530

    Score kipper ui

--- src/App.js
+++ src/App.js
@@ -1,7 +1,81 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+// componnent created
+// importing  img
+import Burger from  './burger.png'
+import './App.css'
+import imgadd from './add.png';
+import imgminius from './minus.png';
 
-export default function App() {
+
+const ScoreCard =()=>{
+
+  const BurgerPrice = 100;
+  const [ Total_Bill , SetBill] = useState( BurgerPrice);
+  const [Quantity , SetQuantity ] = useState(1);
+  const  UpdateQuantity=(type)=>{
+    if(type === "minus" && Quantity >1 ) 
+    {
+      SetQuantity(Quantity-1);
+    }
+    else if (type === "plus"  )
+    {
+      SetQuantity(Quantity+1);
+    }
+
+    // Bill
+    
+  
+   
+  
+  }
+  useEffect(()=> {
+    if (Quantity <10 )
+    {
+      SetBill(BurgerPrice*Quantity);
+    }
+    else if ( Quantity > 10 && Quantity < 20)
+    {
+      const Total = BurgerPrice*Quantity;
+      SetBill (Total-50);
+    }
+  },[Quantity])
+  
+
+
+
+ 
   return (
-    <div>Hlw</div>
+    <>
+    <div className='Holebody' > 
+   
+    <div className='Card'>
+      <img src={Burger} className='Product_image' />
+      <h1 className='Product_title'> Burgur</h1>      
+      <div className='action_Btns'>
+     
+      <img src={imgminius} className='Action_btn' alt='imgminus' onClick={()=> UpdateQuantity("minus")}/>
+
+      <img src={imgadd} className='Action_btn' alt='imgplus' onClick={()=> UpdateQuantity("plus")} />
+      </div>
+
+      <button className='Buybtn'>Order Now</button>
+    
+    </div>
+    </div>
+    </>
+
+  )
+
+  
+}
+
+function App() {
+  return(
+ <div className='Scorecard-Container'>
+  <ScoreCard/>
+  <ScoreCard/>
+ </div>
   )
 }
+
+export default App
--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import '../src/App.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
+  <>
+  < h1 className=' Heading'>Score Kipper </h1> 
     <App />
- 
+    </>
 );
 
 // If you want to start measuring performance in your app, pass a function
